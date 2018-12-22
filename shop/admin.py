1ac1fbeda2b0671e84ade1f12643de47ed03d242
Chen Haowei <chenhw26@mail2.sysu.edu.cn>
2018-12-22 17:14:20 +0800

    调整所有品牌页面的url

--- shop/admin.py
+++ shop/admin.py
@@ -3,15 +3,4 @@ from shop.models import *
 
 # Register your models here.
 admin.site.register(Computer)
-admin.site.register(Shop)
 admin.site.register(Brand)
-admin.site.register(Seller)
-admin.site.register(User)
-admin.site.register(Coupon)
-admin.site.register(Shop_comment)
-admin.site.register(Sell)
-admin.site.register(Buy)
-admin.site.register(mark)
-admin.site.register(computer_comment)
-admin.site.register(like)
-admin.site.register(shopping_cart)
